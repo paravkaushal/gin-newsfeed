10e9a875f37fb7d068755d861346eb545a8881b6
Parav Kaushal <parav.kaushal@velotio.com>
2022-03-03 13:01:44 +0530

    writing simple tests

--- httpd/main.go
+++ httpd/main.go
@@ -2,16 +2,22 @@ package main
 
 import (
 	"fmt"
-	"gin-newsfeed/httpd/handler"
-
-	"github.com/gin-gonic/gin"
+	"gin-newsfeed/platform/newsfeed"
 )
 
 func main() {
-	fmt.Println("We good")
+	// fmt.Println("We good")
 
-	r := gin.Default()
+	// r := gin.Default()
 
-	r.GET("/ping", handler.PingGet())
-	r.Run()
+	// r.GET("/ping", handler.PingGet())
+	// r.Run()
+
+	feed := newsfeed.New()
+
+	fmt.Println(feed)
+
+	feed.Add(newsfeed.Item{"Hello", "What are you upto"})
+
+	fmt.Println(feed)
 }
